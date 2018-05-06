a40e1d688e18f366a41cf588c812eacbda44f4bf
Hubert Gawryś <hubertgawrys@gmail.com>
2018-05-06 15:32:33 +0200

    v

---
+++ src/main/java/pl/hubertgawrys/MyTrainingCenter/models/repositories/BodypartRepository.java
@@ -0,0 +1,17 @@
+package pl.hubertgawrys.MyTrainingCenter.models.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.hubertgawrys.MyTrainingCenter.models.BodypartModel;
+import pl.hubertgawrys.MyTrainingCenter.models.UserModel;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+@Repository
+public interface BodypartRepository extends CrudRepository<BodypartModel, Integer> {
+
+    List<BodypartModel> findAllByUserModelEquals(UserModel usermodel);
+    BodypartModel findByIdEquals(int id);
+}
